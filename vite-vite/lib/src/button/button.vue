84bad45739e7f96c9c40f67f273c2b17d002bdd8
Dave Rivera <daverivera90@gmail.com>
2022-10-19 14:11:44 +0200

    vite to vite

---
+++ vite-vite/lib/src/button/button.vue
@@ -0,0 +1,13 @@
+<template>
+    <button type="button" @click="$emit('on-click')">
+      <slot></slot>
+    </button> 
+</template>
+ 
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default /*#__PURE__*/defineComponent({
+    name: "ds-button"
+});
+</script>
---
+++ vite-vite/lib/src/index.ts
@@ -0,0 +1,17 @@
+import DsButton from './button/button.vue';
+//export * from './button/button.vue';
+//export * from './switch/switch.vue';
+import DsSwitch from './switch/switch.vue';
+//export * from './switch/switch.vue';
+//
+
+
+export {
+  DsSwitch,
+}
+
+export {  
+  DsButton,
+}
+
+export default  () => 'Adl button'
