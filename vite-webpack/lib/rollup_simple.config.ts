358312f3fe1279427477dbafe5970936a3307a84
Dave Rivera <daverivera90@gmail.com>
2022-10-19 13:15:21 +0200

    vite wepback - webpack vite

---
+++ vite-webpack/lib/rollup_simple.config.ts
@@ -0,0 +1,44 @@
+//import typescript from '@rollup/plugin-typescript'
+import typescript from 'rollup-plugin-typescript2';
+import babel from '@rollup/plugin-babel';
+//import vue from 'rollup-plugin-vue2';
+import scss from 'rollup-plugin-scss';
+import vue from 'rollup-plugin-vue'
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    file: 'dist/index.js',
+    //format: 'cjs'
+    format: 'esm',
+    exports: 'named',
+  },
+  external: ['vue'],
+  plugins: [
+    typescript({
+      tsconfig: './tsconfig.json',
+      //useTsconfigDeclarationDir: true,
+      //emitDeclarationOnly: true,
+      //experimentalDecorators: true,
+      //module: 'ES2020'
+    }),
+    //scss(),
+    vue({
+      css: true,
+      //template: {
+        //isProduction: true,
+        //compileTemplate: true,
+        //compiler: require('@vue/compiler-sfc'),
+      //},
+      defaultLang: { 
+        script: 'ts' ,
+        style: 'scss'
+      },
+    }),
+    //babel({
+      //exclude: 'node_modules/**',
+      //extensions: ['.js', '.jsx', '.ts', '.tsx', '.vue'],
+      //babelHelpers: 'bundled',
+    //}),
+  ]
+};
---
+++ webpack-vite/lib/src/index.ts
@@ -0,0 +1,23 @@
+import DsButton from './button/button.vue';
+//export * from './button/button.vue';
+//export * from './switch/switch.vue';
+import DsSwitch from './switch/switch.vue';
+//export * from './switch/switch.vue';
+//
+
+
+export {
+  DsSwitch,
+}
+
+export {  
+  DsButton,
+}
+
+export default  () => 'Adl button'
+
+// export default {
+//   DsSwitch,
+//   DsButton
+// }
+
