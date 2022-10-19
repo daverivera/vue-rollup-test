c0c138f2f606f6aa921009bd9604d140b8ecc80d
Dave Rivera <daverivera90@gmail.com>
2022-10-19 14:20:00 +0200

    vite to vite

--- vite-webpack/lib/src/index.ts
+++ vite-webpack/lib/src/index.ts
@@ -1,5 +1,17 @@
-export { default as DsButton } from './button/button.vue';
+import DsButton from './button/button.vue';
 //export * from './button/button.vue';
 //export * from './switch/switch.vue';
-export { default as DsSwitch } from './switch/switch.vue';
+import DsSwitch from './switch/switch.vue';
 //export * from './switch/switch.vue';
+//
+
+
+export {
+  DsSwitch,
+}
+
+export {  
+  DsButton,
+}
+
+export default  () => 'Adl button'
