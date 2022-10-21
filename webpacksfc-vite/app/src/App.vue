77160e2cce226ca204420a867683ad9b8657c429
Dave Rivera <daverivera90@gmail.com>
2022-10-21 10:22:09 +0200

    webpack sfc to vite

---
+++ webpacksfc-vite/app/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <head>
+            <h1>ADL Test project</h1>
+        </head>
+        <article>
+            <ds-button @click="clickedButtonAction">Increment</ds-button>
+            <span>{{ count }}</span>
+        </article>
+    </div>
+</template>
+ 
+<script lang="ts">
+
+import { DsButton } from '@drivera/lib/dist/index.esm.js'
+/*import { multi } from '@drivera/funcs'*/
+ 
+export default {
+    name: "App",
+    components: {
+        DsButton
+    },
+    data() {
+        return {
+            count: 0,
+        }
+    },
+    methods: {
+        clickedButtonAction(): void {
+            /*const numbers = multi(2*4);*/
+            /*console.log(this, { numbers })*/
+            console.log(this)
+            // this.count += 1;
+        }
+    }
+};
+</script>
---
+++ webpacksfc-vite/lib/webpack.config.ts
@@ -0,0 +1,145 @@
+// @ts-ignore
+// @ts-nocheck
+import * as path from 'path';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import { VueLoaderPlugin } from 'vue-loader';
+import { merge } from 'webpack-merge'
+import { Configuration, WebpackPluginInstance } from 'webpack';
+
+import 'webpack-dev-server';
+
+const baseConfig: Configuration = {
+    mode: 'development',
+    devtool: false,
+    entry: {
+      //index: path.resolve(__dirname, 'index.ts'),
+      index: './src/index.ts'
+    },
+    // output: {
+    //     path: path.resolve(__dirname, './dist'),
+    //     filename: '[name].bundle.js',
+    //     library: {
+    //         type: 'module'
+    //     },
+    //     clean: true,
+    // },
+    // experiments: {
+    //     outputModule: true,
+    // },    
+    resolve: {
+        alias: {
+             vue$: 'vue/dist/vue.esm.js',
+        },
+        extensions: ['*', '.vue', '.js', '.ts'],
+        modules: ['src', 'node_modules']
+    },
+    optimization: {
+        usedExports: true,
+        innerGraph: true,
+        sideEffects: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    customElement: true,
+                    enableTsInTemplate: true,
+                    
+                }
+            },
+            {
+                test: /\.(ts|js)?$/,
+                exclude: [/node_modules/],
+                use: [
+                  {
+                    loader: 'babel-loader',
+                    options: {
+                      presets: ['@babel/preset-env']
+                    }
+                  },
+                  {
+                      loader: 'ts-loader',
+                      options: {
+                          appendTsSuffixTo: [/\.vue$/], // Process .vue files
+                          transpileOnly: true,
+                          configFile: require.resolve('./tsconfig.json'),
+                      },
+                  }
+                ],
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: ['vue-style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.svg$/,
+                use: [
+                    {
+                        loader: 'svg-url-loader',
+                        options: {
+                            limit: 10000,
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    externals: ['vue'],
+    plugins: [
+        new VueLoaderPlugin() as WebpackPluginInstance,
+        new ForkTsCheckerWebpackPlugin({
+            typescript: {
+                extensions: {
+                    vue: {
+                        enabled: true,
+                        compiler: 'vue-template-compiler',
+                    },
+                },
+            },
+        }),
+    ],
+};
+
+const umd = merge(baseConfig, {
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'index.js',
+        library: {
+            type: 'umd'
+        },
+        
+        
+    }
+})
+
+const esm = merge(baseConfig, {
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'index.esm.js',
+        library: {
+            type: 'module'
+        },
+        module: true,
+        
+        environment: { module: true },
+    },
+    experiments: {
+        outputModule: true,
+    }, 
+})
+
+const cjs = merge(baseConfig, {
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'index.cjs.js',
+        library: {
+            type: 'commonjs'
+        },
+        
+    }
+})
+
+export default [esm, umd, cjs];
+// export default esm;
