37335909387ff36ac8e7845599df13cf261b049b
Borys Ponomarenko <bponomarenko@gmail.com>
2022-10-29 18:05:56 +0200

    Rollup-webpack setup for JS with tree-shaking

---
+++ rollup-webpack-js/app/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+
+        <head>
+            <h1>ADL Test project</h1>
+        </head>
+        <article>
+            <ds-button @click="clickedButtonAction">Increment</ds-button>
+            <span>{{ count }}</span>
+        </article>
+    </div>
+</template>
+
+<script>
+import { DsButton } from '@borys/lib';
+
+export default {
+    components: {
+        DsButton
+    },
+    data() {
+        return {
+            count: 0,
+        }
+    },
+    methods: {
+        clickedButtonAction() {
+            console.log(this)
+        }
+    }
+};
+</script>
---
+++ rollup-webpack-js/lib/src/button/button.vue
@@ -0,0 +1,17 @@
+<template>
+  <button type="button" @click="$emit('on-click')">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: "ds-button"
+});
+</script>
+
+<style>
+
+</style>
