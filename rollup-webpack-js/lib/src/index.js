fe8329b7548d2894b1bfd7e5345c7f089da1b9fc
Borys Ponomarenko <bponomarenko@gmail.com>
2022-10-31 18:21:43 +0100

    Add default export to test tree-shaking

--- rollup-webpack-js/lib/src/index.js
+++ rollup-webpack-js/lib/src/index.js
@@ -1,5 +1,13 @@
+import Vue from 'vue';
 import DsButton from './button/button.vue';
 import DsSwitch from './switch/switch.vue';
 
 export { DsButton };
 export { DsSwitch };
+
+export default {
+  install: () => {
+    Vue.component('DsButton', DsButton);
+    Vue.component('DsSwitch', DsSwitch);
+  }
+}
