1bccece98720fa0c6ebd7609f2f998df8cb2bcb1
Dave Rivera <daverivera90@gmail.com>
2022-11-03 10:01:33 +0100

    add lib

--- rollup-webpack-ts/app/src/App.vue
+++ rollup-webpack-ts/app/src/App.vue
@@ -5,18 +5,27 @@
             <h1>ADL Test project</h1>
         </head>
         <article>
-            <ds-button @click="clickedButtonAction">Increment</ds-button>
+            <adl-button @on-click="clickedButtonAction">Increment</adl-button>
             <span>{{ count }}</span>
+            <adl-card>
+                <template #headerActions>
+                    <adl-button variant="secondary">Secondary</adl-button>
+                    <adl-button>Primary</adl-button>
+                </template>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ipsum sem, fermentum ut erat at, porta lacinia sapien. Maecenas venenatis massa justo, ut ornare mi lacinia interdum. Vivamus tristique, lorem id vestibulum suscipit, nulla risus fermentum orci, at ultrices augue ipsum sed purus. Nam facilisis tellus fringilla facilisis blandit. Quisque commodo metus mi. Morbi bibendum elit ut leo aliquet tempus. Vestibulum eu consectetur nulla.
+            </adl-card>
         </article>
     </div>
 </template>
 
 <script>
-import { DsButton } from '@borys/lib';
+import { AdlButton, AdlCard } from '@borys/lib';
+
 
 export default {
     components: {
-        DsButton
+        AdlButton,
+        AdlCard
     },
     data() {
         return {
